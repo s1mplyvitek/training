d0603a52f1e0c8aca6f92a13b3f0d2154e5ef58d
Victor <borismustanger@gmail.com>
2023-06-17 15:21:42 +0800

    useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let qwer = 1;
+  let [a, setA] = useState(123);
+
+  setInterval(() => {
+    setA(a + 10)
+  }, 1000);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {a}
     </div>
   );
 }
